32309d3c6342301a163693e9e6de863b4f07816f
Erwin Brown Pitti <74215573+erwinbrown@users.noreply.github.com>
2020-11-23 14:04:00 -0500

    Aplicacion Interceptor
    Uso de la interface HandlerInterceptor con la clase con anotacion Configuration y la interface  WebMvcConfigurer

---
+++ src/main/java/com/bolsadeideas/springboot/Intercepapp/app/SpringBootInterceptarApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.Intercepapp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInterceptarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInterceptarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bolsadeideas/springboot/Intercepapp/app/SpringBootInterceptarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.Intercepapp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInterceptarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
